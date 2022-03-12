82f27625fcc03a9cd4a7b0e7121aa89bb69c34e7
Hossein <ah.rahimi19219@gmail.com>
2022-03-13 01:48:23 +0330

    Revert "commit 1"
    This reverts commit 868783b0c2a90882ff061b4d61ef1c96a0677d1a.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import './App.css';
+import SignUp from "./View/Signup/SignUp";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/sign-up" component={SignUp}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
