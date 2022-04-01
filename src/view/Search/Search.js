8be478fd6f24f83345f03dfb840200ca33685937
Hossein <ah.rahimi19219@gmail.com>
2022-04-01 16:32:03 +0430

    commit 3

--- src/view/Search/Search.js
+++ src/view/Search/Search.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Input, Select } from 'antd';
+import React, { useState } from 'react'
+import { Input, Select, AutoComplete } from 'antd';
+import axios from 'axios'
 
 import './Search.css'
 import 'antd/dist/antd.min.css';
@@ -7,8 +8,30 @@ import 'antd/dist/antd.min.css';
 const { Search } = Input;
 const { Option } = Select;
 
-const onSearch = value => {
+const Complete = () => {
+    const [options, setOptions] = useState< {value}>([]);
+    const handleSearch = (value) => {
+      setOptions(
+        !value ? [] : [{ value }, { value: value + value }, { value: value + value + value }],
+      );
+    };
+  
+    const onSelect = (value) => {
+      console.log('onSelect', value);
+    };
+
+const onSearch = async (value) => {
     console.log(value);
+    axios.post('https://timino-app.iran.liara.run//api/user/search-timeline', {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                data: value
+            }).then ( function (response) {
+                console.log(response)
+            }).catch ( function (error) {
+                console.log("error: " + error)
+            })
 };
 
 const selectBefore = (
@@ -18,18 +41,24 @@ const selectBefore = (
     </Select>
   );
 
-export default function HomePage() {
     return (
         <div className="search-body">
             <div className="box">
-            <Search
-                size="large"
-                placeholder="input search ..."
-                onSearch={onSearch}
-                enterButton
-                allowClear
-                addonBefore={selectBefore}
-             />
+            <AutoComplete
+                options={options}
+                style={{ width: 200 }}
+                onSelect={onSelect}
+                onSearch={handleSearch}
+                >
+                <Search
+                    size="large"
+                    placeholder="input search ..."
+                    onSearch={onSearch}
+                    enterButton
+                    allowClear
+                    addonBefore={selectBefore}
+                />
+            </AutoComplete>
              </div>
         </div>
     )
