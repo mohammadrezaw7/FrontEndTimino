2ecfc9b87108dd375e3f661aa62ad4324d07f91d
Hossein <ah.rahimi19219@gmail.com>
2022-04-01 19:48:14 +0430

    commit 3.2

--- src/view/Search/Search.js
+++ src/view/Search/Search.js
@@ -6,23 +6,30 @@ import './Search.css'
 import 'antd/dist/antd.min.css';
 
 const onSearch = async (value) => {
+
     console.log(value);
+
     axios.post('https://timino-app.iran.liara.run//api/user/search-timeline', 
     {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Control-Allow-Origin':'*',
-        'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept',
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept',
         'Access-Control-Allow-Methods' : 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        name : value
+
+        name : {value}
+
     }).then ( function (response) {
-        console.log(JSON.stringify(response.data));
+        console.log(JSON.stringify (response.data));
+        
     }).catch ( function (error) {
         console.log("error: " + error)
     })
 };
 
 const { Option } = Select;
+
 const selectBefore = (
+    
     <Select defaultValue="user" style={{ width: '100px' }}>
       <Option value="user" >User</Option>
       <Option value="timeline" >Timeline</Option>
@@ -30,7 +37,9 @@ const selectBefore = (
   );
 
 export default function Search() {
+
     const { Search } = Input;
+
     return (
         <div className="search-body">
             <div className="box">
