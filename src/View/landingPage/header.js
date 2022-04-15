e44eb69b8326ff6a68ab1e015f2cf91c02c0e82d
Hossein <ah.rahimi19219@gmail.com>
2022-04-15 21:47:45 +0430

    commit 4

--- src/View/landingPage/header.js
+++ src/View/landingPage/header.js
@@ -15,8 +15,27 @@ class Header extends React.Component {
         
         return (
             <header>
-            <div>
-                salam
+            <div className="intro-logo jumbo-bg">
+                <h1>welcome to Timino</h1>
+                <h3>Accompany your activities</h3>
+                <img
+          src="https://bootstrapmade.com/demo/themes/eStartup/img/hero-img.png"
+          className=""
+          alt=""
+        />
+                <div className="intro-button">
+          <a href="">Get Started</a>
+        </div>
+        <div className="company-icons">
+          <span className="company-icons__item">
+            <i className="fab fa-apple" />
+            contact us
+          </span>
+          <span className="company-icons__item">
+            <i className="fab fa-google-play" />
+            telegram
+          </span>
+        </div>
             </div>
         </header>
         )
