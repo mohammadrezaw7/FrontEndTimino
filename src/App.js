09bf99c06e65c8a2e49f8c0e0e657681e43f1a1f
Hossein <ah.rahimi19219@gmail.com>
2022-04-20 01:12:19 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import LandingPage from './View/LandingPage/LandingPage'
-
 import './App.css'
 
 export default function App() {
@@ -10,7 +8,7 @@ export default function App() {
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/" component={ LandingPage } />
+                    
                 </Switch>
             </div>
         </Router>
