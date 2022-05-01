a5965d4831a9b5c83ecebe2dad0a3fb113bbc33d
soheil hamzebeigi <45594725+soheilhamzebeigi@users.noreply.github.com>
2022-05-01 20:20:51 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import './App.css';
-import SignUp from "./View/Signup/SignUp";
+import classes from "./App.module.css";
+import Main from "./view/Main";
+import { useState } from "react";
+import { BsXLg, BsJustify } from "react-icons/bs";
+import { Button } from "react-bootstrap";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/sign-up" component={SignUp}/>
-            </Switch>
-        </Router>
-    );
+  const [panelIsOpen, setPanelIsOpen] = useState(false);
+  return (
+    <div className={classes.App}>
+      <Button
+        style={{ marginBottom: "4px" }}
+        onClick={() => setPanelIsOpen((prev) => !prev)}
+        aria-controls="example-collapse-text"
+        aria-expanded={panelIsOpen}
+      >
+        {panelIsOpen ? <BsXLg /> : <BsJustify />}
+      </Button>
+      <Main open={panelIsOpen} />
+    </div>
+  );
 }
 
 export default App;
