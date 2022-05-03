033da8bf34bff9718130dd79fbcf4b3c93f892c3
hossein19219 <ah.rahimi19219@gmail.com>
2022-05-03 08:32:06 +0430

    Update User.js

--- src/view/Profile/User.js
+++ src/view/Profile/User.js
@@ -1,49 +1,5 @@
 import { createContext } from "react";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
-import qs from "qs";
-
-const data = qs.stringify({});
-
-const config = {
-  method: "get",
-  url: "https://timino-application.iran.liara.run//api/user/show/3",
-  headers: {},
-  data,
-};
-
-export default function GetData() {
-  const [userData, setUserData] = useState("");
-
-  useEffect(() => {
-    axios(config)
-      .then((response) => {
-        const jsonData = JSON.stringify(response);
-        setUserData(JSON.parse(jsonData).data.data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }, []);
-  return {userData}
-}
-
-/* TYPES
-export interface userType {
-  title: string;
-  dt1: number;
-  dt2: number;
-  dt3: number;
-  firstName: string;
-  lastName: string;
-  midName: string;
-  gender: string;
-  phone: string;
-  email: string;
-  pass: string;
-}
-*/
 // INITIAL VALUES
 export const userData = {
   username: "janeD",
@@ -59,3 +15,45 @@ export const userData = {
   pass: "password123"
 };
 export const UserMain = createContext(userData);
+
+/*
+import { useEffect, useState } from "react";
+import axios from "axios";
+import qs from "qs";
+const data = qs.stringify({});
+const config = {
+  method: "get",
+  url: "https://timino-application.iran.liara.run//api/user/show/3",
+  headers: {},
+  data,
+};
+export default function GetData() {
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    axios(config)
+      .then((response) => {
+        const jsonData = JSON.stringify(response);
+        setUserData(JSON.parse(jsonData).data.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+  return {userData}
+}
+*/
+/* TYPES
+export interface userType {
+  title: string;
+  dt1: number;
+  dt2: number;
+  dt3: number;
+  firstName: string;
+  lastName: string;
+  midName: string;
+  gender: string;
+  phone: string;
+  email: string;
+  pass: string;
+}
+*/
