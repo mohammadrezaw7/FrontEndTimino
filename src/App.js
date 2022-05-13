aba292a68c2b9053664cd271fbea8f9844a3f1d0
hossein19219 <ah.rahimi19219@gmail.com>
2022-05-13 16:12:48 +0430

    edit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import PublicProfile from './view/ProfilePublic/profilePage'
 import ProfilePage from './view/Profile/profilePage'
 
 import './App.css'
@@ -12,7 +11,6 @@ export default function App() {
             <div>
                 <Switch>
                     <Route exact path="/" component = { ProfilePage } />
-                    <Route exact path="/public" component = { PublicProfile } />
                 </Switch>
             </div>
         </Router>
