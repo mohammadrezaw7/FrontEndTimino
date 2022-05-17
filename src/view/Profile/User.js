d261e4966792eae00e40aa5a4fe386a3cd605e3f
hossein19219 <ah.rahimi19219@gmail.com>
2022-05-17 19:14:19 +0430

    Update User.js

--- src/view/Profile/User.js
+++ src/view/Profile/User.js
@@ -1,59 +1,42 @@
 import { createContext } from "react";
+import axios from 'axios';
+
+
+const data = axios.get('https://timino-app-2.iran.liara.run//api/user/my-profile', {
+
+  headers: {
+    'Authorization' : 'Bearer '+ 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMiIsInR5cGUiOiJBY2Nlc3NUb2tlbiIsImlhdCI6MTY1MDI2OTgwMiwibmJmIjoxNjgxMzczODAyfQ.RdUSlLG33NbaK-7eyB4Gb9t1_dSdDqa_4zxtjfZMdn4',
+    'Content-Type' : 'application/x-www-form-urlencoded'
+  }
+
+}).then(function (response) {
+  console.log(response.data.data)
+  userData.username = response.data.data.username
+  userData.email = response.data.data.email
+  userData.firstName = response.data.data.first_name
+  userData.lastName = response.data.data.last_name
+  userData.midName = response.data.data.username
+  userData.phone = response.data.data.phone
+  userData.pass = response.data.data.password
+  userData.gender = response.data.data.gender
+
+}).catch(function (error) {
+  console.log("error:" + error);
+
+});
 
 // INITIAL VALUES
 export const userData = {
-  username: "janeD",
-  dt1: 32,
-  dt2: 40,
-  dt3: 50,
-  firstName: "Jane",
-  lastName: "Doe",
-  midName: "Baker",
-  gender: "female",
-  phone: "932-555-4247",
-  email: "janedoe@gmail.com",
-  pass: "password123"
+  username: "",
+  firstName: "",
+  lastName: "",
+  midName: "",
+  gender: "",
+  phone: "",
+  email: "",
+  pass: "",
 };
-export const UserMain = createContext(userData);
 
-/*
-import { useEffect, useState } from "react";
-import axios from "axios";
-import qs from "qs";
-const data = qs.stringify({});
-const config = {
-  method: "get",
-  url: "https://timino-application.iran.liara.run//api/user/show/3",
-  headers: {},
-  data,
-};
-export default function GetData() {
-  const [userData, setUserData] = useState("");
-  useEffect(() => {
-    axios(config)
-      .then((response) => {
-        const jsonData = JSON.stringify(response);
-        setUserData(JSON.parse(jsonData).data.data);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }, []);
-  return {userData}
-}
-*/
-/* TYPES
-export interface userType {
-  title: string;
-  dt1: number;
-  dt2: number;
-  dt3: number;
-  firstName: string;
-  lastName: string;
-  midName: string;
-  gender: string;
-  phone: string;
-  email: string;
-  pass: string;
-}
-*/
+console.log({userData})
+
+export const UserMain = createContext(userData);
