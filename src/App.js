bde9da69273fdd7105198cdba0e5bce43304d0b7
soheil hamzebeigi <soheil2000.h@gmail.com>
2022-06-01 18:40:50 +0430

    commit two

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import { useRoutes } from "react-router-dom";
-
-import "./App.css";
-import ProfilePage from "./View/Profile/private/profilePage";
-import PublicProfile from "./View/Profile/public/profilePage";
-import Search2 from "./View/UserCard/Search";
-import ForgetPasswordPage from "./View/forget-password/ForgetPasswordPage";
-import ResetPage from "./View/forget-password/ResetPage";
-import CodePage from "./View/forget-password/Code";
-import CreateTimeline from "./View/CreateTimeLine/CreateTimeline";
-import Main from "./View/NewTimelineView/Main";
-import SignInSignUp from "./View/SignIn-SignUp/SignIn-SignUp";
-import LandingPage from "./landing-page-item/LandingPage";
-import MessageList from "./View/Chat/MessageList";
-import Timelineindex from "./View/Timeline-index/Timelineindex";
-
-
-const RoutePath = () => {
-  let routes = useRoutes([
-    { path: "/", element: <LandingPage /> },
-    { path: "/Chat", element: <MessageList /> },
-    { path: "/dashboard", element: <ProfilePage /> },
-    { path: "/public-profile", element: <PublicProfile /> },
-    { path: "/signup", element: <SignInSignUp /> },
-    { path: "/card", element: <Search2 /> },
-    { path: "/timeline-view/:id", element: <Main /> },
-    { path: "/ForgetPasswordPage", element: <ForgetPasswordPage /> },
-    { path: "/ResetPage", element: <ResetPage /> },
-    { path: "/CodePage", element: <CodePage /> },
-    { path: "/CreateTimeline", element: <CreateTimeline /> },
-    { path: "/Timelineindex", element: <Timelineindex /> },
-
-  ]);
-  return routes;
-};
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import './App.css';
+import SignUp from "./View/Signup/SignUp";
 
 function App() {
-  return <RoutePath />;
+    return (
+        <Router>
+            <Switch>
+                <Route path="/sign-up" component={SignUp}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./store/index";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
