d32a2603aa192b850e60832ddb548a16f60ce985
hossein19219 <ah.rahimi19219@gmail.com>
2022-06-01 17:18:36 +0430

    Update profileCard.js

--- src/View/Profile/private/profileCard.js
+++ src/View/Profile/private/profileCard.js
@@ -99,13 +99,15 @@ export default function ProfileCard() {
           </Button>
         </Grid>
         <Grid item style={styles.details} sx={{ width: "100%" }}>
-          <Button
-            variant="contained"
-            color="error"
-            sx={{ width: "99%", p: 1, my: 2 }}
-          >
-            Logout
-          </Button>
+          <a href="/">
+            <Button
+              variant="contained"
+              color="error"
+              sx={{ width: "99%", p: 1, my: 2 }}
+            >
+              Logout
+            </Button>
+          </a>
         </Grid>
       </Grid>
     </Card>
